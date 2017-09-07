848e320db517b3cfa32c0c1be29ba43a6c1a522c
Raihan Chandra <student@ent-v314-010.local>
2017-09-07 15:49:04 -0400

    added meatgrinder

---
+++ unity-projects/day1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject go;
+
+	public float StartAfter = 1f;
+
+	public float SpawnFrequency = .5f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnThing", StartAfter, SpawnFrequency);
+		
+	}
+	
+	// Update is called once per frame
+	void SpawnThing () {
+
+		transform.position = new Vector3 (Random.Range(-5,5), 3, Random.Range(-5,5));
+
+		Instantiate (go, transform.position, Quaternion.identity);
+	}
+}
