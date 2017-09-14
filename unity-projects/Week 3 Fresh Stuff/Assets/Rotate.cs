16bc58cb81b9b30db27c359241f0a044e8c86eea
Raihan Chandra <student@ent-v314-010.local>
2017-09-14 15:50:50 -0400

    added week 3 stuff

---
+++ unity-projects/Week 3 Fresh Stuff/Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// rotation variable
+	public Vector3 myRotation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(myRotation * Time.deltaTime);
+	}
+}
---
+++ unity-projects/Week 3 Fresh Stuff/Assets/RotateOnHover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnHover : MonoBehaviour {
+
+	// rotation variable
+	public Vector3 myRotation;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void OnMouseOver() {
+		transform.Rotate(myRotation * Time.deltaTime);
+	}
+}
