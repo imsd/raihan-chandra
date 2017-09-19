c9dfe1fcdca6ed6c691d4212deac1feecb6880c0
Raihan Chandra <student@ent-v314-010.local>
2017-09-19 15:49:50 -0400

    added week 4 spinners

---
+++ unity-projects/Week 3 Fresh Stuff/Assets/input.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input : MonoBehaviour {
+
+
+	public float multiplier = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationX = Input.GetAxis ("Horizontal") * multiplier;
+
+		float rotationY = Input.GetAxis ("Vertical") * multiplier;
+
+		Vector3 v3 = new Vector3 (rotationX, rotationY, 0);
+
+		transform.Rotate (v3);
+
+
+		//Debug.Log (rotation);
+	}
+}
