a89476e1e3426373e3752e7d1b45d47b602b9a7c
Raihan Chandra <student@ent-v314-010.local>
2017-09-28 15:56:16 -0400

    project stuff

---
+++ unity-projects/Project Attempt 1/Assets/Scripts/ProjectileMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMouse : MonoBehaviour {
+
+	// what to create on click
+	public GameObject prefab;
+
+	// how far, more or less, do you want it to go?
+	public float distance = 10f;
+
+	// how fast?
+	public float power = 2000f;
+
+	// Update is called once per frame
+	void Update () {
+		// on left click
+		if (Input.GetMouseButtonDown(0)) {
+
+			// converting 2d screen coordinates to 3d "world" coordinates
+			Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+			position = Camera.main.ScreenToWorldPoint(position);
+
+			// spawn an object and give it a push
+			GameObject go = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+			go.transform.LookAt(position);    
+			go.GetComponent<Rigidbody>().AddForce(go.transform.forward * power);
+		}
+	}
+}
---
+++ unity-projects/Project Attempt 1/Assets/Scripts/SpawnerRandom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerRandom : MonoBehaviour {
+
+	public GameObject go;
+	public GameObject go2;
+	public GameObject go3;
+
+	public float StartAfter = 1f;
+
+	public float SpawnFrequency = .5f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnThing", StartAfter, SpawnFrequency);
+
+	}
+
+	// Update is called once per frame
+	void SpawnThing () {
+
+		transform.position = new Vector3 (Random.Range(-5,5), 3, Random.Range(-5,5));
+		int RandomNumber = (int) Random.Range (0, 3);
+		if (RandomNumber == 0) {
+			Instantiate (go, transform.position, Quaternion.identity);
+		}
+		else if (RandomNumber == 1) {
+			Instantiate (go2, transform.position, Quaternion.identity);
+		}
+		else {
+			Instantiate (go3, transform.position, Quaternion.identity);
+		}
+	}
+}
