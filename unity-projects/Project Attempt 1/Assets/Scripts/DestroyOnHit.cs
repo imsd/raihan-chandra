92739c5a5265a7cd268053be64391c50d3566b35
Raihan Chandra <student@ent-v314-010.local>
2017-10-03 15:44:08 -0400

    updated project

---
+++ unity-projects/Project Attempt 1/Assets/Scripts/DestroyOnHit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnHit : MonoBehaviour {
+
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.name == "HitBall(Clone)")
+		{
+			Destroy(collision.gameObject);
+			Destroy(this.gameObject);
+		}
+		else {
+			Destroy (this.gameObject, 3);
+	}
+
+
+}
+}
