657972de505b7b301b23d2f46416495797bfa69d
Raihan Chandra <student@ent-v314-010.local>
2017-10-05 15:46:18 -0400

    added video project

---
+++ unity-projects/VideoProject/Assets/rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+	public Vector3 myRotation;
+
+	// U
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (myRotation *Time.deltaTime);
+	}
+}
